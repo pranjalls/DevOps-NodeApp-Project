229f2b0e439ad417d6ac3533c34b398602120fb6
Pranjal Shinde <110478058+pranjalls@users.noreply.github.com>
2025-07-19 19:44:54 +0530

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
